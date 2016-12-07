49f68f3d867825c0c1e77b322517adeefa2e387b
chriso18 <christian.sierra.elias@gmail.com>
2016-12-06 23:20:09 -0600

    Operacion1
    Raiz Cuadratica

---
+++ Hoja_de_Trabajo_1/Hoja_de_Trabajo_1/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Hoja_de_Trabajo_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Operacion1 = new System.Windows.Forms.Button();
+            this.Operacion2 = new System.Windows.Forms.Button();
+            this.Operacion3 = new System.Windows.Forms.Button();
+            this.Operacion4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Operacion1
+            // 
+            this.Operacion1.Location = new System.Drawing.Point(675, 108);
+            this.Operacion1.Name = "Operacion1";
+            this.Operacion1.Size = new System.Drawing.Size(75, 23);
+            this.Operacion1.TabIndex = 0;
+            this.Operacion1.Text = "Operacion1";
+            this.Operacion1.UseVisualStyleBackColor = true;
+            this.Operacion1.Click += new System.EventHandler(this.Operacion1_Click);
+            // 
+            // Operacion2
+            // 
+            this.Operacion2.Location = new System.Drawing.Point(675, 162);
+            this.Operacion2.Name = "Operacion2";
+            this.Operacion2.Size = new System.Drawing.Size(75, 23);
+            this.Operacion2.TabIndex = 1;
+            this.Operacion2.Text = "Operacion2";
+            this.Operacion2.UseVisualStyleBackColor = true;
+            this.Operacion2.Click += new System.EventHandler(this.Operacion2_Click);
+            // 
+            // Operacion3
+            // 
+            this.Operacion3.Location = new System.Drawing.Point(675, 216);
+            this.Operacion3.Name = "Operacion3";
+            this.Operacion3.Size = new System.Drawing.Size(75, 23);
+            this.Operacion3.TabIndex = 2;
+            this.Operacion3.Text = "Operacion3";
+            this.Operacion3.UseVisualStyleBackColor = true;
+            this.Operacion3.Click += new System.EventHandler(this.Operacion3_Click);
+            // 
+            // Operacion4
+            // 
+            this.Operacion4.Location = new System.Drawing.Point(675, 264);
+            this.Operacion4.Name = "Operacion4";
+            this.Operacion4.Size = new System.Drawing.Size(75, 23);
+            this.Operacion4.TabIndex = 3;
+            this.Operacion4.Text = "Operacion4";
+            this.Operacion4.UseVisualStyleBackColor = true;
+            this.Operacion4.Click += new System.EventHandler(this.Operacion4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Resultado);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.Operacion1);
+            this.panel1.Controls.Add(this.Operacion4);
+            this.panel1.Controls.Add(this.Operacion2);
+            this.panel1.Controls.Add(this.Operacion3);
+            this.panel1.Location = new System.Drawing.Point(31, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(796, 423);
+            this.panel1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(46, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(46, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(221, 69);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(46, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(55, 198);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(55, 13);
+            this.Resultado.TabIndex = 7;
+            this.Resultado.Text = "Resultado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "1er Termino";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "2do Termino";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "3er Termino";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(675, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(58, 237);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(58, 281);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 470);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Operacion1;
+        private System.Windows.Forms.Button Operacion2;
+        private System.Windows.Forms.Button Operacion3;
+        private System.Windows.Forms.Button Operacion4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Hoja_de_Trabajo_1/Hoja_de_Trabajo_1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hoja_de_Trabajo_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void Metodo1() 
+        {
+            double x1, x2;
+
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int c = Convert.ToInt32(textBox3.Text);
+
+            double resultado = Math.Pow(b, 2) * (4 * a * c);
+
+            a = a * 2;
+            if(Math.Sign(resultado)== -1)
+            {
+                resultado = (Math.Sqrt(-resultado))/a;
+                x1 = -b / a;
+                textBox4.Text = x1 + "-" + resultado.ToString();
+                textBox5.Text = x1 + "+" + resultado.ToString();
+
+            }
+            else
+            {
+                resultado = Math.Sqrt(resultado);
+                x1= (-b - resultado)/a;
+                x2= (-b + resultado)/a;
+            }
+           
+        }
+
+        public void Metodo2()
+        {
+            
+        }
+
+        public void Metodo3()
+        {
+
+
+
+
+        }
+
+        public void Metodo4()
+        {
+
+
+
+
+        }
+        
+
+        private void Operacion1_Click(object sender, EventArgs e)
+        {
+            Metodo1();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "";
+        }
+
+        private void Operacion2_Click(object sender, EventArgs e)
+        {
+            Metodo2();
+        }
+
+        private void Operacion3_Click(object sender, EventArgs e)
+        {
+            Metodo3();
+        }
+
+        private void Operacion4_Click(object sender, EventArgs e)
+        {
+            Metodo4();
+        }
+
+
+    }
+}
